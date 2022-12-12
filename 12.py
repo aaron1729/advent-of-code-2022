524a49792f1be2f9ac30f316d4b591f30351fb01
Aaron Mazel-Gee <aaron1729@gmail.com>
2022-12-12 00:44:57 -0800

    still partway through day 12 part 1

--- 12.py
+++ 12.py
@@ -9,6 +9,7 @@ accszExk
 acctuvwj
 abdefghi'''
 
+
 # # un/comment this to toggle example vs actual data
 # str1 = example_str
 
@@ -35,7 +36,7 @@ for (index, sublst) in enumerate(lst2):
 
 print('S is', S)
 print('E is', E)
-print('now, lst2 is', lst2)
+# print('now, lst2 is', lst2)
 print('just for context, the function ord takes the following values:')
 print('ord(a) is', ord('a'))
 print('ord(b) is', ord('b'))
@@ -45,7 +46,7 @@ print('ord(E) is', ord('E'))
 
 lst3 = [[ord(char) for char in sublst] for sublst in lst2]
 
-print('lst3 is', lst3)
+# print('lst3 is', lst3)
 
 ###
 
@@ -77,18 +78,58 @@ def walk(history):
                         walk(history_copy)
 
 
-walk([S])
-print('after calling the walk function, routes is', routes)
 
-# print(len(routes))
-# print(len(routes[0]))
 
-print('answer to part 1 is', min([len(route) for route in routes]) - 1)
+
+# walk([S])
+# print('after calling the walk function, routes is', routes)
+
+# # print(len(routes))
+# # print(len(routes[0]))
+
+# print('answer to part 1 is', min([len(route) for route in routes]) - 1)
+
+
+
 
 
 ### the above function is taking way too long! a speedup: run through all routes at the same rate, keeping track of them in a list, and discard a potential new route if _any_ old route has already reached its newest node.
 
+routes_start = [[S]]
 
+def lengthen_routes(routes):
+    longer_routes = []
+    for route in routes:
+        current = route[len(route) - 1]
+        height_current = lst3[current[0]][current[1]]
+        Deltas = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for Delta in Deltas:
+            # make a copy of the route so we're not affecting the original version
+            route_copy = route.copy()
+            new = tuple(map(sum, zip(current, Delta)))
+            # make sure new is on the grid
+            if new[0] >= 0 and new[0] < len(lst3) and new[1] >= 0 and new[1] < len(lst3[0]):
+                height_new = lst3[new[0]][new[1]]
+                # make sure we can walk to new
+                if (height_new - height_current <= 1):
+                    # make sure we haven't already visited new, in _any_ of the previous routes
+                    visited = False
+                    for any_route in routes:
+                        if new in any_route:
+                            visited = True
+                    if not visited:
+                        route_copy.append(new)
+                        longer_routes.append(route_copy)
+    return longer_routes
 
+def min_route():
+    inner_routes = routes_start
+    while True:
+        print('there are', len(inner_routes), 'viable routes of length', len(inner_routes[0]))
+        # print('now finding routes of length', len(inner_routes[0]) + 1)
+        inner_routes = lengthen_routes(inner_routes)
+        for route in inner_routes:
+            if route[len(route)-1] == E:
+                return len(route) - 1
 
-
+print(min_route())
