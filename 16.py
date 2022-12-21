36a81e45f80758d5deb649acc55cbb538c5d51c3
Aaron Mazel-Gee <aaron1729@gmail.com>
2022-12-20 23:34:18 -0800

    finish day 16 part 1

--- 16.py
+++ 16.py
@@ -17,8 +17,8 @@ Valve JJ has flow rate=21; tunnel leads to valve II'''
 
 
 
-# un/comment this to toggle example vs actual data
-str1 = example_str
+# # un/comment this to toggle example vs actual data
+# str1 = example_str
 
 
 
@@ -104,9 +104,9 @@ for valve in essential_valves_dict:
         if essential_target != valve:
             essential_valves_dict[valve]["essential_targets"][essential_target] = min_dist(valve, essential_target)
 
-print('and now, essential_valves_dict is:', essential_valves_dict)
+# print('and now, essential_valves_dict is:', essential_valves_dict)
 
-# add back in 'AA' to essential_valves_dict, just to make things easy later.
+# # add back in 'AA' to essential_valves_dict, just to make things easy later.
 
 essential_valves_dict_AA = {}
 essential_valves_dict_AA["flow"] = 0
@@ -116,161 +116,54 @@ for valve in essential_valves_dict:
 
 essential_valves_dict["AA"] = essential_valves_dict_AA
 
-print('and now now, essential_valves_dict is:', essential_valves_dict)
+# print('and now now, essential_valves_dict is:', essential_valves_dict)
 
 
 
+# a route is recorded as a list of pairs. each pair contains a valve and a number saying at what minute the valve was opened. the numbers go in increasing order (i.e. the valves are listed in chronological order). all start with ('AA', 0), and valves never repeat.
+
+# we build the routes recursively, and compare their high score against the current best. we do *not* actually record the routes, as there are 14! ~ 87billion of them.
+
+# # in the given optimal solution for the example, valve DD is being opened in minute 2. thus, it is open at minutes 3, 4, 5, ..., 30, i.e. for 28 minutes. its flow rate is 20, so it releases 28 * 20 = 560 units of pressure. so in general, if a valve with flow rate F is being opened at minute M, then it releases (30-M) * F units of pressure.
+
+def route_score(route):
+    score = 0
+    for (valve, time) in route:
+        if time < 30:
+            score += (30-time) * essential_valves_dict[valve]["flow"]
+    return score
+
+solution_to_example = [('AA', 0), ('DD, 2'), ('BB', 5), ('JJ', 9), ('HH', 17), ('EE', 21), ('CC', 24)]
 
 
-
-########################################################
-
-
-
-
-
-
-
-
-
-
-flows = [sublst[1] for sublst in lst]
-
-max_flow = max(flows)
-
-print('max_flow is:', max_flow)
-
-flows_sorted = flows.copy()
-
-flows_sorted.sort(reverse=True)
-
-print('flows_sorted is', flows_sorted)
-
-flows_sorted_filtered = list(filter(lambda flow: flow > 0, flows_sorted))
-
-print('flows_sorted_filtered is', flows_sorted_filtered)
-
-max_remaining_points_possible = []
-
-for i in range(30):
-    L = min(30-i, len(flows_sorted_filtered))
-    output = 0
-    for j in range(L):
-        output += flows_sorted_filtered[j] * (30-i-j)
-    max_remaining_points_possible.append(output)
-
-print('max_remaining_points_possible is', max_remaining_points_possible)
-
-
-
-# create all routes of length N, for N running from 1 to 30. at each stage, find the maximum score thus far, and the maximum number of points possible for the remainder of the time, and throw away all of the routes that have no chance of being optimal.
-
-# a route is recorded as a list of pairs. each pair contains a valve and a boolean. the boolean records whether we are *currently* opening the valve.
-
-base_route = [('AA', False)]
-
-def score(route):
-    output = 0
-    for index, pair in enumerate(route):
-        if pair[1]:
-            output += valves_dict[pair[0]]["flow"] * (30 - index)
-    return output
-
-
-### these are from the example input, not the real input. so they must be commented out when running the code for real.
-
-# example_route_1 = [('AA', False), ('AA', True), ('DD', False), ('DD', True)]
-# print('score of example_route_1 is:', score(example_route_1)) # 540
-
-# example_route_2 = [('AA', False), ('AA', True), ('DD', False), ('CC', False)]
-# print('score of example_route_2 is:', score(example_route_2)) # 0
-
-# example_route_3 = [('AA', False), ('DD', False), ('DD', True), ('CC', False)]
-# print('score of example_route_3 is:', score(example_route_3)) # 560
-# # this is how the given optimal solution in the example begins. the DD valve is open at minutes 3, 4, 5, ..., 30, i.e. for 28 minutes. its flow rate is 20, so it releases 28 * 20 = 560 units of pressure. so this is correct! (not off by one.)
-
-
-
-
-
-routes = [base_route]
-
-essentially_complete_routes = []
 
 high_score = 0
+counter = 0
 
-# a complete route has length 31. (it starts with ('AA', False) at minute 0.)
-    # after 0 iterations (i.e. iterating through range(0)), every route in the routes list has length 1.
-    # after 1 iteration (i.e. iterating through range(1)), every route in the routes list has length 2.
-    # etc.
-# so, we want to iterate through range(30).
-for i in range(0):
-    new_routes = []
-    for route in routes:
-        # print('extending the route:', route)
-        last = route[len(route)-1]
-        opened = [pair[0] for pair in route if pair[1]]
-        # if the flow rate of a given valve is 0, it cannot strictly improve the score to open the valve. so, only open the valves with positive flow rate.
-        if (not last[0] in opened) and (valves_dict[last[0]]["flow"] > 0):
-            new_routes.append(route + [(last[0], True)])
-        for valve in valves_dict[last[0]]["targets"]:
-            new_routes.append(route + [(valve, False)])
-    scores = [score(route) for route in new_routes]
-    high_score = max(max(scores), high_score)
-    new_viable_routes = []
-    for j in range(len(new_routes)):
-        if scores[j] + max_remaining_points_possible[i] >= high_score:
-            new_viable_routes.append(new_routes[j])
-    routes = []
-    for route in new_viable_routes:
-        opened = [pair[0] for pair in route if pair[1]]
-        if len(opened) >= len(flows_sorted_filtered):
-            essentially_complete_routes.append(route)
-        else:
-            routes.append(route)
+def make_routes(route=[('AA', 0)]):
 
-    print('at the end of iteration numbered', i, 'of the for loop (starting with 0), routes has length', len(routes))
-    print('and essentially_complete_routes has length', len(essentially_complete_routes))
-    # print('and their scores are', [score(route) for route in routes])
-    print('and each route has length', len(routes[0]))
-    print('and the maximum score so far is', high_score)
-    print('and we threw away', len(new_routes) - len(new_viable_routes), 'routes because they were not viable')
-    print('\n')
-
-
-
-
-
-
-
-
-
-
-# run through all possible sequences of actions, and add them to a list of routes. each sequence of actions is recorded as a list of 3-tuples containing the valve name, pressure, and time of opening.
-routes = []
-
-
-# the list "output" contains pairs of a valve and a boolean indicating whether we're currently opening it.
-# the set "opened" records (locally!) whether a valve has been opened in a given route, as we're constructing it.
-def create_routes(current='AA', output=[('AA', False)], opened=set()):
-    # print('currently, length of output is:', len(output))
-    if len(output) == 3:
-        routes.append(output)
-        print('just added to routes, and and now its length is:', len(routes))
-        print('and the route added is:', output)
+    end_of_route = route[len(route)-1]
+    current = end_of_route[0]
+    time = end_of_route[1]
+    
+    if (time >= 30) or (len(route) == len(essential_valves_dict)):
+        global counter
+        counter += 1
+        global high_score
+        high_score = max(high_score, route_score(route))
+        print('just found route number', counter, 'and now the new high score is', high_score)
         return
+    
+    for valve in essential_valves_dict[current]["essential_targets"]:
+        visited_valves = [pair[0] for pair in route]
+        if not valve in visited_valves:
+            new_route = route.copy()
+            new_route.append((valve, time + 1 + essential_valves_dict[current]["essential_targets"][valve]))
+            make_routes(new_route)
 
-    last = output[len(output)-1][0]
-    if not (last in opened):
-        new_opened = opened.copy()
-        new_opened.add(last)
-        create_routes(last, output + [(last, True)], new_opened)
+make_routes()
 
-    for valve in valves_dict[last]["targets"]:
-        # fork off into different versions of the "output" list.
-        # print('forking off into different tunnels, and currently output is:', output)
-        create_routes(valve, output + [(valve, False)], opened)
+print('high_score (the answer to part 1) is:', high_score)
 
-# create_routes()
 
-# print('after create_routes, routes length is:', len(routes))
+######################################################
